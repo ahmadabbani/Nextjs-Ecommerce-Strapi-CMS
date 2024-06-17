7f732da7681d9efc5e3f42a038851e00c14fe407
ahmadabbani <ahmdabbanyy@gmail.com>
2024-06-17 19:27:22 +0300

    Fixed query parameter handling in Checkout component

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./_components/CheckoutForm";
-import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 function Checkout() {
-  const searhParams = useSearchParams();
+  const router = useRouter();
+  const { amount } = router.query;
   const options = {
     mode: "payment",
     currency: "usd",
-    amount: Number(searhParams.get("amount")) * 100,
+    amount: Number(amount) * 100,
   };
   return (
     <Elements stripe={stripePromise} options={options}>
